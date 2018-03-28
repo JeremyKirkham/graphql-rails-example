c534960df22469efe71f5f91e579801c6e980197
Jeremy Kirkham <jeremy@streame.com.au>
2018-03-28 18:14:43 +1100

    custom resolver for author full name

--- app/graphql/types/author.rb
+++ app/graphql/types/author.rb
@@ -6,6 +6,13 @@ module Types
     field :last_name, !types.String
     field :posts, types[!PostInterface]
 
+    # Custom resolver for a field
+    field :full_name, !types.String do
+      resolve lambda { |obj, _args, _ctx|
+        "#{obj.first_name} #{obj.last_name}"
+      }
+    end
+
     # Relay style pagination
     # connection :posts, Types::PostInterface.connection_type
   end
